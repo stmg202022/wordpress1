cbbd225a5fa3fa9c4aaa4e50fd06f7e267ebaef9
samsontmg <stmg202023@gmail.com>
2024-01-19 10:27:28 +0545

    added:  my-plugins setup as a new plugins

---
+++ app/public/wp-content/plugins/my-plugin/my-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:        Samson Tmg
+ * Description:       Twenty Twenty-Three is designed to take advantage of the new design tools introduced in WordPress 6.1. With a clean, blank base as a starting point, this default Plugin includes ten diverse style variations created by members of the WordPress community. Whether you want to build a complex or incredibly simple website, you can do it quickly and intuitively through the bundled styles or dive into creation and full customization yourself.
+ * Version:           1.0.0
+ * Author:            Samson Tmg
+ * Author URI:        https://example.com
+ * Plugin URI:         https://example.com/fabled-sunset
+ */
+
